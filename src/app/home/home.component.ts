21692dd88dc0d1de28d7b96b05fc49115ba771ae
ch-folio <chelhuds@gmail.com>
2023-10-21 16:55:41 -0700

    Adding code for basic user interface

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../apparel/product/product.service';
+import {Product} from '../shared/models/Product';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  products:Product[] = [];
+  constructor(private productService:ProductService) { }
+
+  ngOnInit(): void {
+    this.products = this.productService.getAll();
+  }
+
+}
